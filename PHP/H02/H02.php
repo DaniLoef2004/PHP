1e4626dd8df44caf07be1bb49c60422378d86b7c
DaniLoef2004 <70745652+DaniLoef2004@users.noreply.github.com>
2020-10-21 19:10:55 +0200

    Add files via upload

---
+++ PHP/H02/H02.php
@@ -0,0 +1,44 @@
+<?php
+
+$a="Ik heb niet gefaald";
+$b="die niet werken";
+$c="10000";
+
+echo nl2br ($a . ". Ik heb alleen " . $c . " manieren gevonden " . $b . ".");
+
+echo nl2br("\n");
+echo nl2br("\n");
+
+$d ="\"omgaan";
+$e ="met";
+$f ="teleu";
+
+
+echo "De cursus " . $d . " " . $e . " " . $f . "lstellingen\"" . " kan vanavond helaas niet doorgaan.";
+
+echo nl2br("\n");
+echo nl2br("\n");
+
+$g ="ry";
+$h =", but";
+$i ="fail";
+
+echo "T" . $g . " and " . $i . $h . " never " . $i . " to t" . $g . ".";
+
+echo nl2br("\n");
+echo nl2br("\n");
+
+$j ="die geen fout";
+$k ="en";
+$l ="niet.";
+
+echo "Mensen " . $j . $k . " mak" . $k . ", werk" . $k . " " . $l;
+
+echo nl2br("\n");
+echo nl2br("\n");
+?>
+
+
+<a href="../index.php">Inhoudsopgave</a>
+
+
